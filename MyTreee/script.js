1d9a32d42c18c5b3f7053380d3c31395737279c4
Maisa Cardoso <contatomaisacs@gmail.com>
2023-04-22 16:49:05 -0300

    Update and rename play.js to script.js

---
+++ MyTreee/script.js
@@ -0,0 +1,23 @@
+var sza = document.getElementById("sza");
+sza.volume = 0.1;
+
+document.addEventListener("click", (e) => {
+
+  var gato = document.createElement("span");
+  gato.classList.add("gato");
+
+
+  gato.style.left = e.offsetX + "px";
+  gato.style.top = e.offsetY + "px";
+
+
+  var size = Math.random() * (200 - 20 + 1) + 20;
+
+
+  gato.style.width = size + "px";
+  gato.style.height = size + "px";
+  document.body.appendChild(gato);
+  setTimeout(() => {
+    gato.remove();
+  }, 1000);
+});
